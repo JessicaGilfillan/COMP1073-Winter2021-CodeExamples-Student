978d0669fce3f07b253f2251c5a31bcbfe70305b
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2021-02-19 12:31:12 -0500

    delete instructor files

---
+++ week-6/02-working-with-events/js/events.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+
+// STEP ONE:
+/*
+Your Code Explanation Here: 
+*/
+
+
+//STEP TWO:
+/*
+Your Code Explanation Here: 
+*/
+
+
+//STEP THREE 
+/*
+Your Code Explanation Here: 
+*/
+
+
+// STEP FOUR 
+/*
+Your Code Explanation Here: 
+*/
+
+
+//STEP FIVE: 
+/*
+Your Code Explanation Here: 
+*/
+
+
+//STEP SIX:
+/*
+Your Code Explanation Here: 
+*/
+
+
+//STEP SEVEN:
+/*
+Your Code Explanation Here: 
+*/
+
