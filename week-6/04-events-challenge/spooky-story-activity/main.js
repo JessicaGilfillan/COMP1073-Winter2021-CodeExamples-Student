78aba0238e44aa5b908a049ed9a256e43d887e1b
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2021-02-19 12:57:24 -0500

    Update main.js

--- week-6/04-events-challenge/spooky-story-activity/main.js
+++ week-6/04-events-challenge/spooky-story-activity/main.js
@@ -1,6 +1,5 @@
 // JavaScript Document
 var personName = "";
-let button = document.querySelector('button'); 
 
 function complainMessage() {
   alert("I told you not to do that " + personName);
@@ -26,5 +25,16 @@ function finishTheStory() {
   document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!";
 }
 
-button.onclick = complainMessage;
+
+/* When the user clicks the make it spooky button, goDark should be invoked */ 
+
+
+/* When the user types their name in the input, typeAndScream should be invoked */
+
+
+/* When the user clicks the button that says please, please don't click me, complainMessage() should be invoked*/ 
+
+
+/* When the user clicks the tell me what happened button, finish the story should be invoked */
+
 
